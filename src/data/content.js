30fa442e75bfb88479b42dea204c9437c8dddeb1
Sankar Raghuthaman <sankarrag@gmail.com>
2025-11-16 23:43:57 -0500

    Update Data

--- src/data/content.js
+++ src/data/content.js
@@ -87,21 +87,28 @@ export const experienceData = [
       company: "Children's Hospital of Philadelphia",
       role: "Data Science Intern",
       duration: "July 2025 – Present",
-      description: "Working on building machine learning models and streamlining data workflows to support clinical research."
+      description: "Engineered end-to-end Azure ML pipelines for processing 1M+ DNA probe genomic data, applying statistical and ML models to identify key syndrome predictors with 100% recall and ROC-AUC. Automated preprocessing and data standardization workflows, cutting manual tasks by 75%, ensuring reproducible outcomes. Translated insights from 20+ research papers into production-ready Python workflows, reducing analytical turnaround by 50%. Collaborated with cross-functional teams to integrate clinical data, boosting model accuracy and predictive reliability by 25%."
     },
     {
       image: tcslogo,
       company: "Tata Consultancy Services",
-      role: "Data Engineer",
+      role: "Machine Learning Engineer",
       duration: "Aug 2020 – Jul 2024",
-      description: "At TCS, I contributed to migrating core banking systems for Canadian Imperial Bank of Commerce (CIBC) to Azure Cloud, improving performance, scalability, and reducing batch processing time by over 25%. I also designed scalable ETL pipelines with PySpark and Azure Data Factory, integrating real-time FX rate feeds, improving transaction processing by 30%, and optimizing routing logic for on-time completion."
+      description: "Spearheaded migration of core banking systems to Azure Cloud, automating data workflows and reducing downtime by 30%. Build and deployed of GPT-3.5 Turbo–based field extractor for 1,000+ invoices, automating extraction of financial entities (GST, tax, date, amount) with 95% precision and 87% recall. Applied prompt engineering and benchmarking against traditional NER models, delivering a 70% accuracy improvement and reducing manual verification efforts. Mentored 7 junior engineers, standardizing Agile-based ML workflows and improving code quality by 20%."
+    },
+    {
+      image: tcslogo,
+      company: "Tata Consultancy Services",
+      role: "System Engineer",
+      duration: "Aug 2020 – Jul 2024",
+      description: "Designed and implemented a computer vision model integrated with a Flask RESTful API to detect screen elements using OCR and icons, achieving 92% accuracy and optimized performance through threading implementation. Architected ETL pipelines integrating real-time FX rate updates via Azure Data Factory and Spark, improving transaction efficiency by 2x across international operations. Implemented CI/CD automation with Azure DevOps, reducing release cycles by 40% and improving deployment consistency. Defined and validated transformation logic with technical teams to ensure lossless, high-quality data exchange across systems."
     },
     {
       image: eduvancelogo,
       company: "Eduvance Pvt. Ltd.",
       role: "Machine Learning Intern",
       duration: "May 2019 – Jul 2019",
-      description: "Worked on developing Deep Learning models for image processing and enhancement using SRCNN for super-resolution."
+      description: "Designed and optimized a deep learning SRCNN model, boosting image resolution and pixel clarity by 50%. Increased neural network training throughput on HPC clusters by 40% through distributed computing efficiency improvements."
     }
 ];
   
@@ -110,7 +117,7 @@ export const researchData = [
     title: "LLM's to Evaluate and Improve Comments", 
     publisher: "Independent Study Project", 
     date: "Ongoing", 
-    description: "Researching the application of Large Language Models (LLMs) to enhance code commenting for better clarity and insight.",
+    description: "Researching the application of Large Language Models (LLMs) to enhance code commenting for better clarity and insight making onboarding easier.",
     },  
     {
       title: "Classification of Disease using CBC",
